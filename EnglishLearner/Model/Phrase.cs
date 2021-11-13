91febda3858dc56944a52edd6fe2883d19493f7b
Hunter Van de Water <vandhe07@uwgb.edu>
2021-11-13 09:33:16 -0600

    Added some phrase logic for the subject, verb, object

--- EnglishLearner/Model/Phrase.cs
+++ EnglishLearner/Model/Phrase.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using System.Data;
 
 namespace EnglishLearner
 {
     /*
-     * Created by Cole Lamers 
+     * Created by Cole Lamers & Hunter Van de Water
      * Date: 2021-11-04
      * 
      * == Purpose ==
@@ -16,8 +18,11 @@ namespace EnglishLearner
 
     public class Phrase
     {
+
+        Sqlite_Actions _sql; // table is called 'entries'
         public string Phrase_Sentence { get; set; }
         public string[] Phrase_Split_Sentence { get; set; }
+        public string Phrase_First_Word { get; set; }
         public string Phrase_Subject { get; set; }
         public string Phrase_Verb { get; set; }
         public string Phrase_Object { get; set; }
@@ -29,8 +34,11 @@ namespace EnglishLearner
 
         public Phrase(string sentence)
         {
+            // TODO: --3-- could make all interpret functions asyncronously running for each phrase initialized. could speed things up down the road.
+
             this.Phrase_Sentence = sentence;
-            this.Phrase_Split_Sentence = sentence.Split(" "); // TODO: --1-- not sure this works yet
+            this.Phrase_Split_Sentence = interpretSplitSentence();
+            this.Phrase_First_Word = interpretFirstWord(); // relies on Phrase_Split_Sentence to occur before this
             this.Phrase_Subject = interpretSubject();
             this.Phrase_Verb = interpretVerb();
             this.Phrase_Object = interpretObject();
@@ -38,35 +46,81 @@ namespace EnglishLearner
             this.Phrase_Legalties = interpretLegalities();
         } // constructor; Phrase
 
+        private string[] interpretSplitSentence()
+        {
+            string[] array = this.Phrase_Sentence.Split(" ");
+            int num;
+            array = array.Where(x => !string.IsNullOrWhiteSpace(x)).Where(x => !int.TryParse(x, out num)).ToArray();
+
+            return array;
+        }
+
+        private string interpretFirstWord()
+        {
+            // TODO: --1-- running into an issue when parsing the split sentences to find the first letter a space or a number. also if it's only one word. always causes the program to crash around here.
+            char[] capitalizedFirstLetter = this.Phrase_Split_Sentence[0].ToCharArray();
+            capitalizedFirstLetter[0] = char.ToUpper(capitalizedFirstLetter[0]);
+            return new string(capitalizedFirstLetter);
+        } // function interpretFirstWord
+
         private Dictionary<string, bool> interpretLegalities()
         {
+            Dictionary<string, bool> word_legalVal = new Dictionary<string, bool>();
+
+
             // TODO: --1-- interpretLegality logic here; is the word legal (meaning proper English)
             /*
              * Legality of a term is determined by the context of the sentence. Typically can only be determined by a native;
+             * loop through words and determine if they're legal
              */
             // this.Phrase_Legalities.Add("Them", true); this word is legal
             // this.Phrase_Legalities.Add("crazys", false); this word is illegal
-            return tof;
+            return word_legalVal;
         }
 
         //Run queries to find out if an object in the sentece is subject, verb, object, and any punctuation.
         //Test out the insertInto function within the SQLite Actions tab to handle words not found in the database
 
 
-
+        //How do we want to interpret this????
         private char interpretPunctuation()
         {
             char the_punctuation = ' ';
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i].Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            //}
 
+
+            // TODO: --1-- need to remove the punctuation from the sentence as well
             // TODO: --1-- interpretPunctuation logic here
             return the_punctuation;
         } // function; interpretPunctuation
 
+
         private string interpretSubject()
         {
+            // TODO: --1-- interpretSubject logic here
             string the_subject = "";
 
-            // TODO: --1-- interpretSubject logic here
+            var subjectArray = Phrase_Split_Sentence;
+
+            for (int i = 0; i < subjectArray.Length; i++)
+            {
+                _sql.ExecuteQuery("Select word from entries where word='" + subjectArray[0] + "'");
+
+                foreach (DataRow n in _sql.ActiveQueryResults.Rows)
+                {
+                    var x = n["word"];
+                    var y = n["wordtype"];
+
+                    if ((x != null && y != null) && i == 0)
+                    {
+                        the_subject = (string)x;
+                    }
+                }
+            }
+
             return the_subject;
         } // function; interpretSubject
 
@@ -74,6 +128,24 @@ namespace EnglishLearner
         {
             string the_verb = "";
             // TODO: --1-- interpretVerb logic here
+
+            var subjectArray = Phrase_Split_Sentence;
+
+            for (int i = 0; i < subjectArray.Length; i++)
+            {
+                _sql.ExecuteQuery("Select word from entries where word='" + subjectArray[0] + "'");
+
+                foreach (DataRow n in _sql.ActiveQueryResults.Rows)
+                {
+                    var x = n["word"];
+                    var y = n["wordtype"];
+
+                    if ((x != null && y != null) && i == 1)
+                    {
+                        the_verb = (string)x;
+                    }
+                }
+            }
             return the_verb;
         } // function; interpretVerb
 
@@ -81,28 +153,44 @@ namespace EnglishLearner
         {
             string the_object = "";
             // TODO: --1-- interpretObject logic here
+
+            var subjectArray = Phrase_Split_Sentence;
+
+            for (int i = 0; i < subjectArray.Length; i++)
+            {
+                _sql.ExecuteQuery("Select word from entries where word='" + subjectArray[0] + "'");
+
+                foreach (DataRow n in _sql.ActiveQueryResults.Rows)
+                {
+                    var x = n["word"];
+                    var y = n["wordtype"];
+
+                    if ((x != null && y != null) && i == 2)
+                    {
+                        the_object = (string)x;
+                    }
+                }
+            }
             return the_object;
         } // function; interpretObject
 
+        /*
+                // TODO: --4-- Touch at a later time
+                private bool interpretSubjectPlural()
+                {
+                    // TODO: --4-- utilize this function as a basis for other functions when determining tense and other things
+                    // TODO: --4-- this is not correct because words like "US, zealous, cross, pelvis, taxes, grass." Can add a check for 's, ' at the end, double 's', -ous, -ass/-ess/-iss/-oss/-uss
+                    char[] letterArray = this.Phrase_Subject.ToLower().ToCharArray();
+                    // TODO: --4-- we could also utilize regex for this instead
+                    // NOTE: char array is faster than looping through the entire array since we know that plurality in English is defined by the last few characters typically when context is not established
+                    if (letterArray[letterArray.Length - 1].Equals('s'))
+                    {
+                        return true;
+                    } // if;
 
+                    return false;
+                } // function; interpretSubjectPlural
+        */
 
-
-
-
-        // TODO: --4-- Touch at a later time
-        //private bool interpretSubjectPlural()
-        //{
-        //    // TODO: --4-- utilize this function as a basis for other functions when determining tense and other things
-        //    // TODO: --4-- this is not correct because words like "US, zealous, cross, pelvis, taxes, grass." Can add a check for 's, ' at the end, double 's', -ous, -ass/-ess/-iss/-oss/-uss
-        //    char[] letterArray = this.Phrase_Subject.ToLower().ToCharArray();
-        //    // TODO: --4-- we could also utilize regex for this instead
-        //    // NOTE: char array is faster than looping through the entire array since we know that plurality in English is defined by the last few characters typically when context is not established
-        //    if (letterArray[letterArray.Length - 1].Equals('s'))
-        //    {
-        //        return true;
-        //    } // if;
-
-        //    return false;
-        //} // function; interpretSubjectPlural
     }
 }
